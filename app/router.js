c6a50e0c7eef38f398e16370d7a66cc98f9c41f7
nai08747 <nia.maheshwari@kub.org>
2025-02-13 07:18:25 -0500

    fix: fix the commit issues encountered in GitHub GUI.

--- app/router.js
+++ app/router.js
@@ -2,13 +2,13 @@ import EmberRouter from '@ember/routing/router';
 import config from 'super-rentals/config/environment';
 
 export default class Router extends EmberRouter {
-    location = config.locationType;
-    rootURL = config.rootURL;
-};
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
-Router.map(function() {
-    this.route('rentals'); //should map rentals page to '/'
-    this.route('about'); //tells Ember router to run the about.js
-			//file when visitor navigates to about page
-    this.route('contact');
+Router.map(function () {
+  this.route('rentals'); //should map rentals page to '/'
+  this.route('about'); //tells Ember router to run the about.js
+  //file when visitor navigates to about page
+  this.route('contact');
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,10 @@
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
-
 export default class IndexRoute extends Route {
+  @service router;
 
-@service router;
-
-    beforeModel() {
-	this.router.transitionTo('rentals');
-    }
+  beforeModel() {
+    this.router.transitionTo('rentals');
+  }
 }
-
-
